8300ecf2df4041664bc545cbcdae910839c1b9ed
Greg Guthe <gguthe@mozilla.com>
2019-05-20 12:22:54 -0400

    gh meta: unnest and remove field names containing '.'

--- github_metadata_fetcher/github_metadata_fetcher/serializer.py
+++ github_metadata_fetcher/github_metadata_fetcher/serializer.py
@@ -23,19 +23,19 @@ def serialize_repo(repo):
     }
 
     # assume there aren't too enough of items in array fields to exceed
-    # BigQuerie's 100MB row size limit
+    # BigQuery's 100MB row size limit
     row["languages"] = list(serialize_repo_langs_iter(repo))
-    row["languages.totalCount"] = len(row["languages"])
-    row["languages.totalSize"] = repo.languages.totalSize  # in bytes
+    row["languages_count"] = len(row["languages"])
+    row["languages_bytes"] = repo.languages.totalSize
 
     row["dependencyGraphManifests"] = list(serialize_repo_manifests_iter(repo))
-    row["dependencyGraphManifests.totalCount"] = len(row["dependencyGraphManifests"])
+    row["dependencyGraphManifests_count"] = len(row["dependencyGraphManifests"])
 
     row["dependencies"] = list(serialize_repo_manifest_deps_iter(repo))
-    row["dependencies.totalCount"] = len(row["dependencies"])
+    row["dependencies_count"] = len(row["dependencies"])
 
     row["vulnerabilityAlerts"] = list(serialize_repo_vuln_alerts_iter(repo))
-    row["vulnerabilityAlerts.totalCount"] = len(row["vulnerabilityAlerts"])
+    row["vulnerabilityAlerts_count"] = len(row["vulnerabilityAlerts"])
 
     return row
 
@@ -89,28 +89,26 @@ def serialize_vuln_alert(edge):
         if not field.startswith("__")
         and type(getattr(edge.node, field, Sentinal)) in SCALAR_TYPES
     }
-    row.update(serialize_advisory(edge.node.securityAdvisory))
-    row.update(serialize_vuln_alert_dismisser(edge.node))
+    row["securityAdvisory"] = serialize_advisory(edge.node.securityAdvisory)
+    row.update(serialize_vuln_alert_dismissal(edge.node))
     return row
 
 
 def serialize_advisory(advisory):
     row = {
-        "securityAdvisory." + field: getattr(advisory, field, Sentinal)
+        field: getattr(advisory, field, Sentinal)
         for field in dir(advisory)
         if not field.startswith("__")
         and type(getattr(advisory, field, Sentinal)) in SCALAR_TYPES
     }
-    row[
-        "securityAdvisory.severity"
-    ] = advisory.severity.value  # .value since it's an enum
-    row["securityAdvisory.identifiers"] = [
+    row["severity"] = advisory.severity.value  # .value since it's an enum
+    row["identifiers"] = [
         {"type": sa_id.type, "value": sa_id.value} for sa_id in advisory.identifiers
     ]
-    row["securityAdvisory.vulnerabilities"] = [
+    row["vulnerabilities"] = [
         n for n in serialize_vulns_iter(advisory.vulnerabilities.nodes)
     ]
-    row["securityAdvisory.referenceUrls"] = [
+    row["referenceUrls"] = [
         getattr(r, "url", None) for r in getattr(advisory, "references", [])
     ]
     return row
@@ -118,14 +116,20 @@ def serialize_advisory(advisory):
 
 def serialize_vulns_iter(vulns):
     for vuln in vulns:
+        first_patched_version = getattr(vuln, "firstPatchedVersion", None)
+        package = getattr(vuln, "package", None)
+
         row = {
-            "firstPatchedVersion.identifier": getattr(vuln, "firstPatchedVersion", None)
-            and getattr(vuln.firstPatchedVersion, "identifier", None),
-            "package.ecosystem": getattr(vuln, "package", None)
-            and getattr(vuln.package, "ecosystem", None)
-            and vuln.package.ecosystem.value,
-            "package.name": getattr(vuln, "package", None)
-            and getattr(vuln.package, "name", None),
+            "firstPatchedVersion": {
+                "identifier": first_patched_version
+                and getattr(first_patched_version, "identifier", None)
+            },
+            "package": {
+                "ecosystem": package
+                and getattr(package, "ecosystem", None)
+                and package.ecosystem.value,
+                "name": package and getattr(package, "name", None),
+            },
             "severity": getattr(vuln, "severity", None) and vuln.severity.value,
             "updatedAt": getattr(vuln, "updatedAt", None),
             "vulnerableVersionRange": getattr(vuln, "vulnerableVersionRange", None),
@@ -133,18 +137,15 @@ def serialize_vulns_iter(vulns):
         yield row
 
 
-def serialize_vuln_alert_dismisser(vuln_alert):
-    dismisser = getattr(vuln_alert, "dismisser", None)
-
+def serialize_vuln_alert_dismissal(vuln_alert):
     row = {}
     for field in ["dismissedAt", "dismissReason"]:
         row[field] = getattr(vuln_alert, field, None)
 
-    for field in ["dismisser.id", "dismisser.name"]:
-        if dismisser:
-            row[field] = getattr(dismisser, field.split(".", 1)[-1], None)
-        else:
-            row[field] = None
+    dismisser = getattr(vuln_alert, "dismisser", None)
+    row["dismisser"] = {
+        getattr(dismisser, field, None) for field in ["id", "name"] if dismisser
+    }
     return row
 
 
