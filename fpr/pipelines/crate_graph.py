61f81cddc583c076403b659f6cdb98926edd7059
Greg Guthe <gguthe@mozilla.com>
2019-08-08 16:03:38 -0400

    add Pipeline models to extant pipelines

--- fpr/pipelines/crate_graph.py
+++ fpr/pipelines/crate_graph.py
@@ -8,6 +8,7 @@ import rx
 import rx.operators as op
 
 from fpr.rx_util import on_next_save_to_file
+from fpr.models import Pipeline
 from fpr.serialize_util import extract_fields, get_in
 from fpr.pipelines.util import exc_to_str
 
@@ -16,6 +17,8 @@ log = logging.getLogger("fpr.pipelines.crate_tree")
 pipeline_name = name = "crate_tree"
 pipeline_reader = reader = lambda infile: [json.load(infile)]
 pipeline_writer = writer = on_next_save_to_file
+__doc__ = """Parses the output of the cargo metadata pipeline and writes a .dot
+file of the dependencies to outfile"""
 
 
 def cargo_metadata_pipeline_to_networkx_digraph(cargo_meta_out: Dict) -> "Digraph":
@@ -56,3 +59,14 @@ def serialize(g: "DiGraph"):
     pdot = to_pydot(g)
     pdot.set("rankdir", "LR")
     return str(pdot)
+
+
+pipeline = Pipeline(
+    name="crate_graph",
+    desc=__doc__,
+    fields=set(),
+    reader=lambda infile: [json.load(infile)],
+    runner=run_pipeline,
+    serializer=serialize,
+    writer=on_next_save_to_file,
+)
