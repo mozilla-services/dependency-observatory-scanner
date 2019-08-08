6a1a305ef16697c1e387079ef5256676d2d26f5f
Greg Guthe <gguthe@mozilla.com>
2019-08-08 13:24:53 -0400

    rx_util only json encode and save dicts to tmp files

--- fpr/rx_util.py
+++ fpr/rx_util.py
@@ -3,7 +3,7 @@ import functools
 import json
 import logging
 import tempfile
-from typing import Any
+from typing import Dict
 
 import rx
 import rx.operators as op
@@ -24,8 +24,12 @@ def map_async(func, *args, **kwds):
     return op.flat_map(do_async(func, *args, **kwds))
 
 
-def save_to_tmpfile(prefix: str, item: Any):
+def save_to_tmpfile(prefix: str, item: Dict):
     "Serializes item to JSON and saves it to a named temp file with the given prefix"
+    if not isinstance(item, Dict):
+        log.debug("skipped saving non-dict {} item to temp file".format(type(item)))
+        return
+
     with tempfile.NamedTemporaryFile(
         mode="w+", encoding="utf-8", prefix=prefix, delete=False
     ) as tmpout:
