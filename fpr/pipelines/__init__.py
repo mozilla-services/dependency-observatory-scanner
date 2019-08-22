d0c255e0f20f791fabe363ccb5ed548a8299a2bf
Greg Guthe <gguthe@mozilla.com>
2019-08-22 15:47:41 -0400

    pipelines: add rust_changelog

--- fpr/pipelines/__init__.py
+++ fpr/pipelines/__init__.py
@@ -2,6 +2,7 @@ from fpr.pipelines.cargo_audit import pipeline as cargo_audit
 from fpr.pipelines.cargo_metadata import pipeline as cargo_metadata
 from fpr.pipelines.crate_graph import pipeline as crate_graph
 from fpr.pipelines.find_git_refs import pipeline as find_git_refs
+from fpr.pipelines.rust_changelog import pipeline as rust_changelog
 
 
-__all__ = [cargo_audit, cargo_metadata, crate_graph, find_git_refs]
+__all__ = [cargo_audit, cargo_metadata, crate_graph, find_git_refs, rust_changelog]
