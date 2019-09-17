48052e0e683c47623d7ffb1a2a4ff8f07acc6294
Greg Guthe <gguthe@mozilla.com>
2019-09-17 16:03:47 -0400

    bin: require hashes and use lockfile for install

--- bin/install.sh
+++ bin/install.sh
@@ -5,9 +5,9 @@ test -d venv || python -m venv venv
 
 echo "installing"
 
-(source venv/bin/activate && pip install -r requirements.txt)
+(source venv/bin/activate && pip install --require-hashes -r requirements.txt.lock)
 DEV=${DEV:-"0"}
 
 if [ "$DEV" = "1" ]; then
-   (source venv/bin/activate && pip install -r dev-requirements.txt)
+   (source venv/bin/activate && pip install --require-hashes -r dev-requirements.txt.lock)
 fi
