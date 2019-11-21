bd8181cb7660598961cb4261ea88f06928ff836f
Greg Guthe <gguthe@mozilla.com>
2019-11-21 17:44:11 -0500

    bump to python 3.8
    for asyncio.Task.get_name() support

--- bin/update_requirements.sh
+++ bin/update_requirements.sh
@@ -10,7 +10,7 @@ python -m venv venv
 touch requirements.txt.lock
 # TODO: figure out a cleaner way to pass these
 # shellcheck disable=SC2046
-./bin/in_venv.sh hashin --verbose --python-version 3.7 --requirements-file=requirements.txt.lock $(./bin/in_venv.sh pip freeze | tr '\n' ' ')
+./bin/in_venv.sh hashin --verbose --python-version 3.8 --requirements-file=requirements.txt.lock $(./bin/in_venv.sh pip freeze | tr '\n' ' ')
 
 # clean the venv so we only install dev requirements
 rm -rf venv dev-requirements.txt.lock
@@ -19,4 +19,4 @@ python -m venv venv
 ./bin/in_venv.sh pip install -r dev-requirements.txt
 touch dev-requirements.txt.lock
 # shellcheck disable=SC2046
-./bin/in_venv.sh hashin --verbose --python-version 3.7 --requirements-file=dev-requirements.txt.lock $(./bin/in_venv.sh pip freeze | tr '\n' ' ')
+./bin/in_venv.sh hashin --verbose --python-version 3.8 --requirements-file=dev-requirements.txt.lock $(./bin/in_venv.sh pip freeze | tr '\n' ' ')
