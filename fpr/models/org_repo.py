d2dc695e70d2aba97d05feb118c74bd094abadf6
Greg Guthe <gguthe@mozilla.com>
2019-12-09 15:38:12 -0500

    models: rm ununsed fields on OrgRepo

--- fpr/models/org_repo.py
+++ fpr/models/org_repo.py
@@ -6,14 +6,6 @@ from typing import Any, Dict, Tuple, Sequence, Generator, Optional, List
 class OrgRepo:
     org: str
     repo: str
-    languages: List = field(default_factory=list)
-    dep_files: List = field(default_factory=list)
-    dep_file_deps: Dict = field(default_factory=dict)
-
-    # map of manifest/dep_file_id to the query params (end cursor and page
-    # size) to fetch it (since GH's GQL API doesn't let us query by node id
-    # yet)
-    dep_file_query_params: dict = field(default_factory=dict)
 
     @property
     def github_clone_url(self) -> str:
