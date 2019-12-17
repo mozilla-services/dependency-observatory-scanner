47cc383093622db717fcc2cfc0caac1be83513df
Greg Guthe <gguthe@mozilla.com>
2019-12-17 12:45:12 -0500

    serialize util: get_in returns None for type mismatch
    instead of throwing an assertion error
    
    to handle empty repository responses

--- tests/unit/test_serialize_util.py
+++ tests/unit/test_serialize_util.py
@@ -13,27 +13,27 @@ import context
 import fpr.serialize_util as m
 
 
+default_get_in_obj = {"a": 1, "b": {"foo": [-1, {}]}}
+
+
 @pytest.mark.parametrize(
-    "path,default,expected",
+    "value,path,default,expected",
     [
-        ([], None, {"a": 1, "b": {"foo": [-1, {}]}}),
-        ([-7], "not found", "not found"),
-        (["a"], None, 1),
-        (["b", "foo", 1], None, {}),
-        (["b", "foo", 1], None, {}),
+        (default_get_in_obj, [], None, {"a": 1, "b": {"foo": [-1, {}]}}),
+        (default_get_in_obj, [-7], "not found", "not found"),
+        (default_get_in_obj, ["a"], None, 1),
+        (default_get_in_obj, ["b", "foo", 1], None, {}),
+        (default_get_in_obj, ["b", "foo", 1], None, {}),
+        ("", [""], None, None),
+        (set(), [-1], None, None),
     ],
 )
-def test_get_in(path, default, expected):
-    assert m.get_in({"a": 1, "b": {"foo": [-1, {}]}}, path, default) == expected
+def test_get_in(value, path, default, expected):
+    assert m.get_in(value, path, default) == expected
 
 
 @pytest.mark.parametrize(
-    "value,path,default,expected_error",
-    [
-        ("", [""], None, AssertionError),
-        (set(), [-1], None, AssertionError),
-        ({}, [set()], None, NotImplementedError),
-    ],
+    "value,path,default,expected_error", [({}, [set()], None, NotImplementedError)]
 )
 def test_get_in_errors(value, path, default, expected_error):
     with pytest.raises(expected_error):
