6bb17983e6f4c73bdc7e73cb7820c358a786534b
Greg Guthe <gguthe@mozilla.com>
2020-01-07 12:44:12 -0500

    models: use f-string for OrgRepo gh clone url

--- fpr/models/org_repo.py
+++ fpr/models/org_repo.py
@@ -9,7 +9,7 @@ class OrgRepo:
 
     @property
     def github_clone_url(self) -> str:
-        return "https://github.com/{0.org}/{0.repo}.git".format(self)
+        return f"https://github.com/{self.org}/{self.repo}.git"
 
     @staticmethod
     def from_org_repo(org_repo):
