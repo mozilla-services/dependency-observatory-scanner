5ad9c0ecfa6cb879e131b5a6d3d493768f8cb4c1
Greg Guthe <gguthe@mozilla.com>
2020-02-21 15:43:00 -0500

    pipelines: save package graphs

--- fpr/pipelines/save_to_db.py
+++ fpr/pipelines/save_to_db.py
@@ -174,6 +174,7 @@ def insert_package_graph(session: sqlalchemy.orm.Session, task_data: Dict) -> No
             package_manager_version=None,
         )
     )
+    session.commit()
 
 
 def insert_package_audit(session: sqlalchemy.orm.Session, task_data: Dict) -> None:
