b83a82986d9ff3bf8739c40a03a94b7b67ef67f1
Greg Guthe <gguthe@mozilla.com>
2020-02-20 22:47:09 -0500

    add postgres db w/ schema

---
+++ fpr/db/connect.py
@@ -0,0 +1,16 @@
+import argparse
+import contextlib
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+@contextlib.contextmanager
+def create_session(engine: sqlalchemy.engine.Engine) -> sqlalchemy.orm.Session:
+    Session = sessionmaker(bind=engine)
+    try:
+        session = Session()
+        yield session
+    finally:
+        session.close()
