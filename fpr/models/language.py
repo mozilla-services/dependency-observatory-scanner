73610abd3ea68c3aa10d548ce5f575a8d18f75d1
Greg Guthe <gguthe@mozilla.com>
2020-02-21 14:54:49 -0500

    models: switch nodejs image to 10-buster-slim to fetch git commits
    for a git version 2.20.1 as opposed to 2.11.0 on node:10 ubuntu

--- fpr/models/language.py
+++ fpr/models/language.py
@@ -246,7 +246,7 @@ package_manager_names = [pm.name for pm in package_managers.values()]
 
 docker_images: Dict[str, DockerImage] = {
     "dep-obs/node-10:latest": DockerImage(
-        base=DockerImageName(None, "node", "10"),
+        base=DockerImageName(None, "node", "10-buster-slim"),
         local=DockerImageName("dep-obs", "node-10", "latest"),
         dockerfile_template="""FROM {base.repo_name}:{base.tag}
 RUN apt-get -y update && apt-get install -y git
