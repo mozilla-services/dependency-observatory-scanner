89b215bd293bf488b4c88b1680b8c3b6158dab21
Greg Guthe <gguthe@mozilla.com>
2020-02-26 11:36:36 -0500

    bin: add analyze (rust) repo script

--- bin/analyze_repo.sh
+++ bin/analyze_repo.sh
@@ -19,5 +19,5 @@ printf '{"repo_url": "%s"}\n' "${repo_url}" \
 	| docker run --rm -i -v /var/run/docker.sock:/var/run/docker.sock "${IMAGE_NAME}" python fpr/run_pipeline.py -v find_git_refs | tee "repo_tags.jsonl" \
 	| docker run --rm -i -v /var/run/docker.sock:/var/run/docker.sock "${IMAGE_NAME}" python fpr/run_pipeline.py -v find_dep_files | tee "repo_dep_files.jsonl" \
 	| docker run --rm -i -v /var/run/docker.sock:/var/run/docker.sock "${IMAGE_NAME}" python fpr/run_pipeline.py -v run_repo_tasks --docker-pull --docker-build --repo-task list_metadata --repo-task audit | tee "repo_tasks.jsonl" \
-        | docker run --rm -i "${IMAGE_NAME}" python fpr/run_pipeline.py -v postprocess --repo-task list_metadata --repo-task audit | tee "repo_postprocessed_tasks.jsonl"
+        | docker run --rm -i "${IMAGE_NAME}" python fpr/run_pipeline.py -v postprocess --repo-task list_metadata --repo-task audit | tee "repo_postprocessed_tasks.jsonl" \
 	| docker run --rm -i -v /var/run/docker.sock:/var/run/docker.sock "${IMAGE_NAME}" python fpr/run_pipeline.py -v rust_changelog | tee "repo_changelog.jsonl"
