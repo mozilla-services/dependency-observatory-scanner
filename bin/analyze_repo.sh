44327092f6ee048a1aa29af4493854f730db3edc
Greg Guthe <gguthe@mozilla.com>
2020-02-26 11:39:33 -0500

    bin: pass DB_URL env var through to save to db in analyze scripts

--- bin/analyze_repo.sh
+++ bin/analyze_repo.sh
@@ -10,8 +10,10 @@ repo_url=$1  # e.g.
 
 # the optional env vars:
 #
+# DB_URL the postgres database URL
 # IMAGE_NAME specifies which docker image to run. Defaults to "mozilla/dependencyscan:latest" (use "fpr:build" for a local image build)
 #
+DB_URL=${DB_URL:-"postgresql+psycopg2://postgres:postgres@localhost/dependency_observatory"}
 IMAGE_NAME=${IMAGE_NAME:-"mozilla/dependencyscan:latest"}
 
 # optionally add --docker-pull --docker-build --save-to-tmpfile to
@@ -23,4 +25,4 @@ printf '{"repo_url": "%s"}\n' "${repo_url}" \
 	| docker run --rm -i -v /var/run/docker.sock:/var/run/docker.sock "${IMAGE_NAME}" python fpr/run_pipeline.py -v find_dep_files | tee "repo_dep_files.jsonl" \
 	| docker run --rm -i -v /var/run/docker.sock:/var/run/docker.sock "${IMAGE_NAME}" python fpr/run_pipeline.py -v run_repo_tasks --docker-pull --docker-build --repo-task list_metadata --repo-task audit | tee "repo_tasks.jsonl" \
         | docker run --rm -i "${IMAGE_NAME}" python fpr/run_pipeline.py -v postprocess --repo-task list_metadata --repo-task audit | tee "repo_postprocessed_tasks.jsonl" \
-        | docker run --rm -i --net=host "${IMAGE_NAME}" python fpr/run_pipeline.py -v save_to_db --input-type postprocessed_repo_task
+        | docker run --rm -i --env DB_URL --net=host "${IMAGE_NAME}" python fpr/run_pipeline.py -v save_to_db --input-type postprocessed_repo_task
