cfffcb3b7db4c40648238d97cdd34be57c31b26d
Greg Guthe <gguthe@mozilla.com>
2020-03-25 17:09:25 -0400

    format setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ from setuptools import setup, find_packages
 
 __dirname = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(__dirname, 'README.md')) as readme:
+with open(os.path.join(__dirname, "README.md")) as readme:
     README = readme.read()
 
-with open(os.path.join(__dirname, 'requirements.txt.lock')) as requirements:
+with open(os.path.join(__dirname, "requirements.txt.lock")) as requirements:
     INSTALL_REQUIRES = [
-        line.split(' ')[0] for i, line in enumerate(requirements.read().replace('\\\n', '').split('\n'))
+        line.split(" ")[0]
+        for i, line in enumerate(requirements.read().replace("\\\n", "").split("\n"))
         if line
     ]
 
@@ -21,25 +22,25 @@ with open(os.path.join(__dirname, 'requirements.txt.lock')) as requirements:
 setup(
     name=NAME,
     version=VERSION,
-    description='Dependency Observatory Scanner: a scanner for software packages and dependencies',
+    description="Dependency Observatory Scanner: a scanner for software packages and dependencies",
     url=SOURCE_URL,
     long_description=README,
     long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Security',
-        'Topic :: Software Development :: Quality Assurance',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Security",
+        "Topic :: Software Development :: Quality Assurance",
     ],
-    author='Greg Guthe',
-    author_email='foxsec+dependencyscan@mozilla.com',
+    author="Greg Guthe",
+    author_email="foxsec+dependencyscan@mozilla.com",
     install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
